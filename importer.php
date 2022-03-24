d21c58e68b78c5c13d0ff2e1cc7b4988f980e4b6
Nimrod-F <foldvari_nimrod@yahoo.com>
2022-03-24 17:17:23 +0200

    add category and simple products

--- importer.php
+++ importer.php
@@ -1,7 +1,7 @@
 <?php
-
-define( 'FILE_TO_IMPORT', 'products.json' );
-
+ini_set('memory_limit', '1024M');
+define( 'FILE_TO_IMPORT', 'data.json' );
+define( 'DATA_LIMIT', 10000);
 require __DIR__ . '/vendor/autoload.php';
 
 use Automattic\WooCommerce\Client;
@@ -26,142 +26,41 @@ $woocommerce = new Client(
 try {
 
 	$json = parse_json( FILE_TO_IMPORT );
-	createCategories();
-	// Import Attributes
-	
-	foreach ( get_attributes_from_json( $json ) as $product_attribute_name => $product_attribute ) :
+	$all_categories = createCategories();
 
-		$products_attributes = $woocommerce->get('products/attributes');
-		
-		$wc_attribute = findObjectByName($products_attributes, $product_attribute_name);
-		
-		if(!findObjectByName($products_attributes, $product_attribute_name)){
-					
-			$attribute_data = array(
-				'name' => $product_attribute_name,
-				'slug' => 'pa_' . strtolower( $product_attribute_name ),
-				'type' => 'select',
-				'order_by' => 'menu_order',
-				'has_archives' => true
-			);
-
-			$wc_attribute = $woocommerce->post( 'products/attributes', $attribute_data );
-			status_message( 'Attribute added. Name: '. $wc_attribute -> name );
-		}
-		
-
-		if ( $wc_attribute ) :
-
-
-			// store attribute ID so that we can use it later for creating products and variations
-			$added_attributes[$product_attribute_name]['id'] = $wc_attribute -> id;
-			
-			// Import: Attribute terms
-			foreach ( $product_attribute['terms'] as $term ) :
-				$wc_attribute_term = findObjectByName($woocommerce->get( 'products/attributes/'. $wc_attribute -> id .'/terms'), $term);
-					if(!$wc_attribute_term) {
-					$attribute_term_data = array(
-						'name' => $term
-					);
-					$wc_attribute_term = $woocommerce->post( 'products/attributes/'. $wc_attribute -> id .'/terms', $attribute_term_data );
-					status_message( 'Attribute term added. Name: '. $wc_attribute -> name);
-				}
-				if ( $wc_attribute_term ) :
-					
-					// store attribute terms so that we can use it later for creating products
-					$added_attributes[$product_attribute_name]['terms'][] = $term;
-				endif;	
-				
-			endforeach;
-
-		endif;		
-
-	endforeach;
-
-
-	$data = get_products_and_variations_from_json( $json, $added_attributes );
-
-	// Merge products and product variations so that we can loop through products, then its variations
-	$product_data = merge_products_and_variations( $data['products'], $data['product_variations'] );
-	$all_products = $woocommerce->get('products');
+	$data = get_products_from_json( $json, $all_categories );
+	$page = 1;
+	$products = [];
+	$all_products = [];
+		do{
+			try {
+				$products = $woocommerce->get('products',array('per_page' => 100, 'page' => $page));
+			}catch(HttpClientException $e){
+				die("Can't get products: $e");
+			}
+			$all_products = array_merge($all_products,$products);
+			$page++;
+		} while (count($products) > 0);
 	// Import: Products
-	foreach ( $product_data as $k => $product ) :
-		if ( isset( $product['variations'] ) ) :
-			$_product_variations = $product['variations']; // temporary store variations array
-
-			// Unset and make the $product data correct for importing the product.
-			unset($product['variations']);
-		endif;
-		if($product['type'] !== 'product_variation') {
+	foreach ( $data as $k => $product ) :
+		
 		$productExist = checkProductById($all_products, $product);
 			if (!$productExist['exist']) {
 				$wc_product = $woocommerce->post('products', $product);
-				status_message( 'Product added. ID: '. $wc_product -> id );
+				status_message( 'Product added. SKU: '. $wc_product -> sku );
+				array_push($all_products, $wc_product);
 		   } else {
 			   /*Update product information */
 			   $idProduct = $productExist['idProduct'];
 			   $wc_product = $woocommerce->put('products/' . $idProduct, $product);
-			   status_message( 'Product updated. ID: '. $wc_product -> id );
+			   status_message( 'Product updated. SKU: '. $wc_product -> sku );
 		   }
-		}
-		if ( isset( $_product_variations ) ) :
-			// Import: Product variations
-			$all_variations = $woocommerce->get('products/' . $wc_product -> id . '/variations');
-			
-			// Loop through our temporary stored product variations array and add them
-			foreach ( $_product_variations as $variation ) :
-				
-				$variationExist = checkVariationById($all_variations, $variation['sku']);
-				if (!$variationExist['exist']) {
-					$wc_variation = $woocommerce->post( 'products/'. $wc_product -> id .'/variations', $variation );
-					status_message( 'Product variation added. ID: '. $wc_variation -> id . ' for product ID: ' . $wc_product -> id );
-			   } else {
-				   
-				   /*Update product information */
-				   $idVariation = $variationExist['id'];
-				   $woocommerce->put( 'products/'. $wc_product -> id .'/variations/' . $idVariation, $variation);
-				   status_message( 'Product variation updated. ID: '. $wc_product -> id . ' for product ID: ' . $variation['_parent_product_id'] );
-			   }
-	
-			endforeach;	
-
-			// Don't need it anymore
-			unset($_product_variations);
-		endif;
 
 	endforeach;
 	
 
 } catch ( HttpClientException $e ) {
-    echo $e->getMessage(); // Error message
-}
-
-/**
- * Merge products and variations together. 
- * Used to loop through products, then loop through product variations.
- *
- * @param  array $product_data
- * @param  array $product_variations_data
- * @return array
-*/
-function merge_products_and_variations( $product_data = array(), $product_variations_data = array() ) {
-	foreach ( $product_data as $k => $product ) :
-		foreach ( $product_variations_data as $k2 => $product_variation ) :
-			if ( $product_variation['_parent_product_id'] == $product['product_id'] ) :
-
-				// Unset merge key. Don't need it anymore
-				// unset($product_variation['_parent_product_id']);
-
-				$product_data[$k]['variations'][] = $product_variation;
-
-			endif;
-		endforeach;
-
-		// Unset merge key. Don't need it anymore
-		// unset($product_data[$k]['_product_id']);
-	endforeach;
-
-	return $product_data;
+    status_message( $e->getMessage() . " " . $product["sku"]); // Error message
 }
 
 /**
@@ -171,147 +70,42 @@ function merge_products_and_variations( $product_data = array(), $product_variat
  * @param  array $added_attributes
  * @return array
 */
-function get_products_and_variations_from_json( $json, $added_attributes ) {
+function get_products_from_json( $json, $all_categories) {
 	$woocommerce = getWoocommerceConfig();
 	$product = array();
-	$product_variations = array();
-	$all_categories = $woocommerce->get('products/categories');
 	foreach ( $json as $key => $pre_product ) :
 		$imagesFormated = array();
 		$imgCounter = 0;
 		$categoriesIds = array();
-		if ( $pre_product['type'] == 'simple' ) :
-			$product[$key]['product_id'] = (int) $pre_product['product_id'];
-			$product[$key]['sku'] = (string) $pre_product['product_id'];
-			$product[$key]['name'] = (string) $pre_product['name'];
-			$product[$key]['description'] = (string) $pre_product['description'];
-			$product[$key]['regular_price'] = (string) $pre_product['regular_price'];
+
+			$product[$key]['product_id'] = (int) $pre_product['ProductID'];
+			$product[$key]['sku'] = (string) $pre_product['CodOnline'];
+			$product[$key]['name'] = (string) $pre_product['Nume'];
+			$product[$key]['description'] = (string) $pre_product['Descriere'];
+			$product[$key]['short_description'] = (string) $pre_product['Brand'];
+			$product[$key]['regular_price'] = (string) $pre_product['Pret SillyCode'];
 			$product[$key]['type'] = 'simple';
-			$categories = $pre_product['categories'];
+			$categories = explode (",", $pre_product['Categorii']);
 			/* Prepare categories */
 			foreach ($categories as $category) {
 				$categoriesIds[] = ['id' => getCategoryIdByName($all_categories, $category)];
 			}
-			$images = $pre_product['pics'];
-			foreach ($images as $image) {
-				$imagesFormated[] = [
-					'src' => $image,
-					'position' => $imgCounter
-				]; /* TODO: FIX POSITON */
-				$imgCounter++;
-			}
-			$product[$key]['images'] = $imagesFormated;
-			$product[$key]['categories'] = $categoriesIds;
-			// Stock
-			$product[$key]['manage_stock'] = (bool) $pre_product['manage_stock'];
-
-			if ( $pre_product['stock'] > 0 ) :
-				$product[$key]['in_stock'] = (bool) true;
-				$product[$key]['stock_quantity'] = (int) $pre_product['stock'];
-			else :
-				$product[$key]['in_stock'] = (bool) false;
-				$product[$key]['stock_quantity'] = (int) 0;
-			endif;	
-
-		elseif ( $pre_product['type'] == 'variable' ) :
-			$product[$key]['product_id'] = (int) $pre_product['product_id'];
-			$product[$key]['sku'] = (string) $pre_product['product_id'];
-			$product[$key]['type'] = 'variable';
-			$product[$key]['name'] = (string) $pre_product['name'];
-			$product[$key]['description'] = (string) $pre_product['description'];
-			$product[$key]['regular_price'] = (string) $pre_product['regular_price'];
-			$categories = $pre_product['categories'];
-			/* Prepare categories */
-			foreach ($categories as $category) {
-				$categoriesIds[] = ['id' => getCategoryIdByName($all_categories, $category)];
-			}
-			$product[$key]['categories'] = $categoriesIds;
-			$images = $pre_product['pics'];
-			foreach ($images as $image) {
+			$images = explode (",", $pre_product['Imagini']);
+			foreach ((array) $images as $image) {
 				$imagesFormated[] = [
 					'src' => $image,
 					'position' => $imgCounter
 				];
 				$imgCounter++;
 			}
+			$product[$key]['stock_status'] = (string) $pre_product['Status'];
 			$product[$key]['images'] = $imagesFormated;
-			// Stock
-			$product[$key]['manage_stock'] = (bool) $pre_product['manage_stock'];
-
-			if ( $pre_product['stock'] > 0 ) :
-				$product[$key]['in_stock'] = (bool) true;
-				$product[$key]['stock_quantity'] = (int) $pre_product['stock'];
-			else :
-				$product[$key]['in_stock'] = (bool) false;
-				$product[$key]['stock_quantity'] = (int) 0;
-			endif;	
-
-			$attribute_name = $pre_product['attribute_name'];
-
-			$product[$key]['attributes'][] = array(
-					'id' => (int) $added_attributes[$attribute_name]['id'],
-					'name' => (string) $attribute_name,
-					'position' => (int) 0,
-					'visible' => true,
-					'variation' => true,
-					'options' => $added_attributes[$attribute_name]['terms']
-			);
-
-		elseif ( $pre_product['type'] == 'product_variation' ) :	
-			$product_variations[$key]['sku'] = (string) $pre_product['id'];
-			$product_variations[$key]['_parent_product_id'] = (int) $pre_product['parent_product_id'];
-
-			$product_variations[$key]['description'] = (string) $pre_product['description'];
-			$product_variations[$key]['regular_price'] = (string) $pre_product['regular_price'];
-
-			// Stock
-			$product_variations[$key]['manage_stock'] = (bool) $pre_product['manage_stock'];
-
-			if ( $pre_product['stock'] > 0 ) :
-				$product_variations[$key]['in_stock'] = (bool) true;
-				$product_variations[$key]['stock_quantity'] = (int) $pre_product['stock'];
-			else :
-				$product_variations[$key]['in_stock'] = (bool) false;
-				$product_variations[$key]['stock_quantity'] = (int) 0;
-			endif;
-
-			$attribute_name = $pre_product['attribute_name'];
-			$attribute_value = $pre_product['attribute_value'];
-
-			$product_variations[$key]['attributes'][] = array(
-				'id' => (int) $added_attributes[$attribute_name]['id'],
-				'name' => (string) $attribute_name,
-				'option' => (string) $attribute_value
-			);
-
-		endif;		
+			$product[$key]['categories'] = $categoriesIds;
+	
 	endforeach;		
 
-	$data['products'] = $product;
-	$data['product_variations'] = $product_variations;
-
-	return $data;
+	return $product;
 }	
-
-/**
- * Get attributes and terms from JSON.
- * Used to import product attributes.
- *
- * @param  array $json
- * @return array
-*/
-function get_attributes_from_json( $json ) {
-	$product_attributes = array();
-
-	foreach( $json as $key => $pre_product ) :
-		if ( !empty( $pre_product['attribute_name'] ) && !empty( $pre_product['attribute_value'] ) ) :
-			$product_attributes[$pre_product['attribute_name']]['terms'][] = $pre_product['attribute_value'];
-		endif;
-	endforeach;		
-
-	return $product_attributes;
-
-}
 
 /**
  * Parse JSON file.
@@ -354,21 +148,11 @@ function findObjectByName($attributes_array, $attribute){
 function checkProductById($products, $p)
 {
     foreach ($products as $product) {
-        if ($product -> sku == $p['product_id']) {
+        if ($product -> sku == $p['sku']) {
             return ['exist' => true, 'idProduct' => $product -> id];
         }
     }
     return ['exist' => false, 'idProduct' => null];
-}
-
-function checkVariationById($variations, $id)
-{
-    foreach ($variations as $variant) {
-        if ($variant -> sku == $id) {
-            return ['exist' => true, 'id' => $variant -> id];
-        }
-    }
-    return ['exist' => false, 'id' => null];
 }
 
 function getCategoryIdByName($categories, $categoryName)
@@ -384,26 +168,41 @@ function createCategories()
 {
 	$woocommerce = getWoocommerceConfig();
     $categoryValues = getCategories();
+	$all_categories = [];
+	$page = 1;
+	do{
+		try {
+			$categories = $woocommerce->get('products/categories', array('per_page' => 100, 'page' => $page));
+		}catch(HttpClientException $e){
+			die("Can't get products: $e");
+		}
+		$all_categories = array_merge($all_categories,$categories);
+		$page++;
+	} while (count($categories) > 0);
     foreach ($categoryValues as $value) {
-		$categories = $woocommerce->get('products/categories');
-        if (!checkCategoryByname($categories, $value["name"])) {
+        if (!checkCategoryByName($all_categories, $value["name"])) {
+			
 			$parentId = 0;
 			if($value["parent"] !== 0){
-				$parentId = findCategoryParentId($categories, $value["parent"]);
+				$parentId = findCategoryParentId($all_categories, $value["parent"]);
 			}
 			$data = array(
 				'name' => $value["name"],
-				'parent' => $parentId
+				'parent' => $parentId,
+				'display' =>  $value["display"]
 			);
             $wc_category = $woocommerce->post('products/categories', $data);
-			status_message( 'Category added. Name: '. $wc_category -> name );
+			status_message("Category added with name: " . $wc_category -> name);
+			array_push($all_categories, $wc_category);
 
         }
     }
+	return $all_categories;
 }
 
 function checkCategoryByName($categories, $categoryName)
 {
+
     foreach ($categories as $category) {
         if ($category -> name == $categoryName) {
             return true;
@@ -417,17 +216,16 @@ function checkCategoryByName($categories, $categoryName)
 function getCategories()
 {
     $products = parse_json( FILE_TO_IMPORT );
-    $categories = array_column($products, 'categories');
-    foreach ($categories as $categoryItems) {
-		for($i = 0; $i < count($categoryItems); ++$i) {
-			if($i == 0 ){
-				$categoryPlainValues[] = ['name' => $categoryItems[$i], 'parent' => $i];
-			}
-			else {
-				$categoryPlainValues[] = ['name' => $categoryItems[$i], 'parent' => $categoryItems[$i-1]];
-			}
-		}
-    }
+    $categories = array_column($products, 'Categorie Principala');
+	foreach ( $products as $key => $pre_product ) :
+		$categorie_principala = (string) $pre_product['Categorie Principala'];
+		$categoryPlainValues[] = ['name' => $categorie_principala, 'parent' => 0, 'display' => 'default'];
+		$categorie_secundara = (string) $pre_product['Categorie Secundara'];
+		if(!empty($categorie_secundara)):
+			$categoryPlainValues[] = ['name' => $categorie_secundara, 'parent' => $categorie_principala, 'display' => 'subcategories'];
+		endif;
+	endforeach;		
+	
     $categoryList = array_unique($categoryPlainValues, SORT_REGULAR);
     return $categoryList;
 }
